8e1d055d5871d1c3aa630f6775fb707858e0561b
Goodluck Kalu <182907427+Goodluck-Kalu@users.noreply.github.com>
2026-07-06 19:41:40 +0100

    initial commit for the weather

---
+++ logic.js
@@ -0,0 +1,37 @@
+const btn= document.getElementById("srchBtn");
+API_KEY="85a6ea6c13574c009cc173115260607";
+btn.addEventListener("click", async function(){
+    const city = document.getElementById("city").value;
+    const name=document.getElementById("cityName");
+    const description = document.getElementById("description")
+    const tempreture = document.getElementById("tempreture");
+    const icon = document.getElementById("icon");
+
+    if(!city){
+        description.textContent ="Input city";
+        return;
+    }
+
+    try{
+        const response =await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}` )
+
+        if(!response.ok){
+             const errData = await response.json();
+             throw new Error(errData.error.message);
+
+        }
+
+        const data =await response.json();
+        console.log(data);
+
+        name.textContent = data.location.name;
+        tempreture.textContent = `${data.current.temp_c}°C`;
+        description.textContent = data.current.condition.text;
+        icon.src = `https:${data.current.condition.icon}`;
+
+    }catch(error){
+        console.error("the exact error is: ",error);
+        description.textContent="something went wrong";
+    }
+
+})
