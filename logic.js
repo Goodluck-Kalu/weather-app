53bc8cdc900a66b81d2cf72bf5781ce90c0bc603
Goodluck Kalu <182907427+Goodluck-Kalu@users.noreply.github.com>
2026-07-06 21:22:08 +0100

    i added a couple features figure it out mehn 😪

--- logic.js
+++ logic.js
@@ -1,6 +1,7 @@
 const btn= document.getElementById("srchBtn");
 API_KEY="85a6ea6c13574c009cc173115260607";
 btn.addEventListener("click", async function(){
+    btn.textContent = "Searching...";
     const city = document.getElementById("city").value;
     const name=document.getElementById("cityName");
     const description = document.getElementById("description")
@@ -17,7 +18,7 @@ btn.addEventListener("click", async function(){
 
         if(!response.ok){
             throw new Error("City not found. Please check your spelling.");
-
+            btn.textContent = "Search";
         }
 
         const data =await response.json();
@@ -27,13 +28,34 @@ btn.addEventListener("click", async function(){
         temperature.textContent = `${data.current.temp_c}°C`;
         description.textContent = data.current.condition.text;
         icon.src = `https:${data.current.condition.icon}`;
+        
 
+        const condition = data.current.condition.text.toLowerCase();
+
+        if (condition.includes("rain")) {
+             document.body.style.backgroundColor = "#4a5568"; // Rainy gray
+        } else if (condition.includes("sunny") || condition.includes("clear")) {
+            document.body.style.backgroundColor = "#f6ad55"; // Sunny orange/yellow
+        } else {
+            document.body.style.backgroundColor = "#2d3748"; // Default dark mode
+        }
+        btn.textContent = "Search";
     }catch(error){
         console.error("the exact error is: ",error);
         description.textContent="something went wrong";
         name.textContent = "";
         temperature.textContent = "";
-        icon.src = "";
+        icon.removeAttribute("src");
+        btn.textContent = "Search";
     }
 
+})
+
+const input = document.getElementById("city");
+
+input.addEventListener("keydown" ,function(event){
+    if(event.key === "Enter"){
+        btn.click();
+
+    }
 })
